c143cc4f58c3eee9b4f3cf4314712fc80b8c7b49
karliky <karliky@gmail.com>
2017-12-09 15:55:41 +0100

    minor changes

---
+++ src/renderer/store/modules/camera.js
@@ -0,0 +1,18 @@
+const state = {
+  speed: 0,
+};
+
+const mutations = {
+  SET_SPEED(speed) {
+    state.speed = speed;
+  },
+};
+
+const actions = {
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+};
