2603da66cf23ed982c08a508396a2c9b1b4642dc
Karliky <karliky@gmail.com>
2020-05-06 18:14:09 +0200

    move text to settings component

--- src/renderer/components/sections/laboratory.vue
+++ src/renderer/components/sections/laboratory.vue
@@ -8,13 +8,6 @@
         <div class="container">
           <h1 class="title">laboratory control.</h1>
           <h2 class="subtitle">Nothing implemented yet.</h2>
-          <p>World of Warcraft® and Blizzard Entertainment® are 
-            all trademarks or registered trademarks of Blizzard 
-            Entertainment in the United States and/or other 
-            countries. These terms and all related materials, 
-            logos, and images are copyright © Blizzard 
-            Entertainment. This app is in no way associated with 
-            or endorsed by Blizzard Entertainment®.”</p>
         </div>
       </div>
     </section>
--- src/renderer/components/sections/settings.vue
+++ src/renderer/components/sections/settings.vue
@@ -39,6 +39,13 @@
           </label>
         </div>
       </div>
+      <small><i>World of Warcraft® and Blizzard Entertainment® are 
+            all trademarks or registered trademarks of Blizzard 
+            Entertainment in the United States and/or other 
+            countries. These terms and all related materials, 
+            logos, and images are copyright © Blizzard 
+            Entertainment. This app is in no way associated with 
+            or endorsed by Blizzard Entertainment®.”</i></small>
     </div>
   </div>
 </template>
